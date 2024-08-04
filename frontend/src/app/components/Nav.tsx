eea19bdf5153d34b15913d94b659df31724773b2
mxber2022 <mxber2022@gmail.com>
2024-08-04 07:22:12 +0200

    components

---
+++ frontend/src/app/components/Nav.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+function Nav() {
+    return(
+        <>
+            <w3m-button />
+        </>
+    )
+}
+
+export default Nav;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Nav from "./components/Nav";
 
 export default function Home() {
   return (
     <>
-    <w3m-button />
+      <Nav></Nav>
     </>
   );
 }
