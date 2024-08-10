708bcd1ad4cc4ec05a704bfd4636387d036f895c
mxber2022 <mxber2022@gmail.com>
2024-08-10 05:13:34 +0200

    update

--- frontend/src/app/components/MarketList/GetmarketList.tsx
+++ frontend/src/app/components/MarketList/GetmarketList.tsx
@@ -8,6 +8,7 @@ import myconfig from '../../../myconfig.json';
 import { Address } from 'viem';
 import { parseEther } from 'viem';
 import "./MarketList.css";
+import { useState } from 'react';
 
 const MY_QUERY = gql`
   query MyQuery {
@@ -32,15 +33,16 @@ interface MyQueryData {
 function GetmarketList() {
   const { loading, error, data } = useQuery<MyQueryData>(MY_QUERY);
   const { writeContract } = useWriteContract();
+  const [amount, setAmount] = useState('');
 
   const handleButtonClick = useCallback(
-    async (marketId: string, outcome: string) => {
+    async (marketId: string, outcome: string, amount: string) => {
       writeContract({
         abi,
         address: myconfig.CONTRACT_ADDRESS_BASE as Address,
         functionName: 'placeBet',
         args: [marketId, outcome],
-        value: parseEther('0.0001')
+        value: parseEther(amount)
       });
     },
     [writeContract]
@@ -56,10 +58,24 @@ function GetmarketList() {
         <div className='marketList__grid'>
           {data?.marketCreateds.map((market) => (
           <div key={market.marketId} className='marketList__card'>
-            <h3>{market.question}</h3>
+            <h3 className="market-question">{market.question}</h3>
+            
+            <div className='betAmount'>
+              <div>
+                <h3>Amount</h3>
+              </div>
+              <div className='amountText'>
+              <input
+                onChange={(e) => setAmount(e.target.value)}
+                required
+                style={{ backgroundColor: 'black', color: 'white', border: '1px solid #ccc', padding: '5px', borderRadius: '5px',}}
+              />
+              </div>
+            </div>
+      
             <div className='marketList__cardBtns'>
               {market.outcomes.split(',').map((outcome, index) => (
-                <button key={index} onClick={() => handleButtonClick(market.marketId, outcome)}>
+                <button key={index} onClick={() => handleButtonClick(market.marketId, outcome, amount)}>
                   {outcome}
                 </button>
               ))}
